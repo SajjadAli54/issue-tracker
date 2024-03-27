a109ff401493ca7c5388a7256829664469c34e2b
Sajjad Ali <khaskhelisajjadali6@gmail.com>
2024-03-27 15:30:25 -0700

    Fix Caching Issues

--- app/issues/_components/IssueForm.tsx
+++ app/issues/_components/IssueForm.tsx
@@ -42,6 +42,7 @@ const IssueForm = ({ issue }: { issue?: Issue }) => {
       if (issue) await axios.patch(`/api/issues/${issue?.id}`, data);
       else await axios.post("/api/issues", data);
       router.push(`/issues`);
+      router.refresh();
       setError("");
     } catch (error) {
       setError("An error occurred while creating the issue " + error);
